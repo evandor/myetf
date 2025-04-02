c434d3b4b7c3b4daf0a4391ac4e8339bdd5cced7
Carsten Gräf <evandor@gmail.com>
2025-04-02 08:01:27 +0200

    initial

---
+++ src/main/java/io/skysail/myetf/MyetfApplication.java
@@ -0,0 +1,13 @@
+package io.skysail.myetf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyetfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyetfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/skysail/myetf/MyetfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.skysail.myetf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyetfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
